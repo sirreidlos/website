ea62de6eecfa7338f098e50d1ca26929c5261e1c
Erik Sanjaya <sirreidlos@gmail.com>
2025-09-25 23:21:16 +0700

    add walkdir because idk what's wrong with shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use axum::Router;
 use tower_http::services::ServeDir;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+use walkdir::WalkDir;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
+    for entry in WalkDir::new("assets").into_iter().filter_map(Result::ok) {
+        let path = entry.path();
+        if path.is_file() {
+            info!("Found file: {}", path.display());
+        } else if path.is_dir() {
+            info!("Found dir: {}", path.display());
+        }
+    }
+
     // ServeDir falls back to serve index.html when requesting a directory
     let router = Router::new().fallback_service(ServeDir::new("assets"));
 
